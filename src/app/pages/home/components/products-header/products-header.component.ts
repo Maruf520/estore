0b4a2b474bdb2c92e5f2c9f03cb0f182fdbae315
Maruf520 <md.maruf5201@gmail.com>
2024-05-01 21:46:20 +0600

    sorting method and button added to product header component

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ProductsHeaderComponent implements OnInit {
-
+  @Output() colNumsCountChange = new EventEmitter<number>();
+ sort = 'desc';
+ itemsShowCount = 12;
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSortUpdate(newSort: string){
+    this.sort = newSort;
+  }
 
+  onCountUpdate(newCount: number)
+  {
+    this.itemsShowCount = newCount;
+  }
+  onColumnsUpdated(colsNum: number): void
+  {
+    this.colNumsCountChange.emit(colsNum);
+  }
 }
