bc8927da640a57727e8028320a1159d673c67977
Maruf520 <md.maruf5201@gmail.com>
2024-05-03 11:13:06 +0600

    Filter Component and Product-box components are added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: {[id: number]: number} = {1: 400, 3: 335, 4: 350};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   constructor() { }
 
   ngOnInit(): void {
   }
   onColNumsCountChange(colsNum: number){
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
-
+  onShowCategory(category: string){
+    console.log(category);
+  }
 }
