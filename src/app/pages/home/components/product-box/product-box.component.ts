652b7ab7c1dc0ae05b4e42ecec7b10858d15c71b
Maruf520 <md.maruf5201@gmail.com>
2024-05-06 22:44:31 +0600

    FakeStoreApi added and getting data from API to UI

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, NgModule, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  NgModule,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { Product } from 'src/app/models/product.models';
 
 @Component({
@@ -6,21 +13,12 @@ import { Product } from 'src/app/models/product.models';
   templateUrl: 'product-box.component.html',
 })
 export class ProductBoxComponent implements OnInit {
-@Input() fullWidthMode = false;
-@Output() addToCart = new EventEmitter();
+  @Input() fullWidthMode = false;
+  @Output() addToCart = new EventEmitter();
 
-product: Product | undefined = {
-id: 1,
-title: 'Snikers',
-price: 150,
-category: 'shoes',
-description: 'Description',
-image: "https://via.placeholder.com/150"
-};
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() product: Product | undefined;
+  constructor() {}
+  ngOnInit(): void {}
   onAddToCart(): void {
     this.addToCart.emit(this.product);
   }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.models';
 import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
 
-const ROWS_HEIGHT: {[id: number]: number} = {1: 400, 3: 335, 4: 350};
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   cols = 3;
+  products: Array<Product> | undefined;
   rowHeight = ROWS_HEIGHT[this.cols];
-  constructor(private cartService: CartService) { }
+  sort = 'desc';
+  count = '12';
+  productsSubscription: Subscription | undefined;
+
+  constructor(
+    private cartService: CartService,
+    private storeService: StoreService
+  ) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
-  onColNumsCountChange(colsNum: number){
+  onColNumsCountChange(colsNum: number) {
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT[this.cols];
   }
-  onShowCategory(category: string){
+  onShowCategory(category: string) {
     console.log(category);
   }
   onAddToCart(product: Product) {
@@ -29,7 +39,17 @@ export class HomeComponent implements OnInit {
       name: product.title,
       price: product.price,
       quantity: 1,
-      id: product.id
+      id: product.id,
     });
+  }
+  getProducts(): void {
+    this.productsSubscription = this.storeService
+      .getAllProducts(this.count, this.sort)
+      .subscribe((_products) => (this.products = _products));
+  }
+  ngOnDestroy(): void {
+    if (this.productsSubscription) {
+      this.productsSubscription.unsubscribe();
+    }
   }
 }
