e9559e833e24dce563393fe1ca0c85c501fe1520
Maruf520 <md.maruf5201@gmail.com>
2024-05-06 10:37:46 +0600

    cart quantity increase decrease functionality added to cart component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,45 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { isEmpty } from 'rxjs';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: 'cart.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class CartComponent implements OnInit {
-cart: Cart = {items:[{
-  product: 'https://via.placeholder.com/150',
-  name: 'Snikers',
-  price: 150,
-  id: 1,
-  quantity: 10
-},{
-  product: 'https://via.placeholder.com/150',
-  name: 'Snikers',
-  price: 150,
-  id: 1,
-  quantity: 7
-}]};
-dataSource: Array<CartItem> = [];
-  constructor(private _carService: CartService) { }
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'Snikers',
+        price: 150,
+        id: 1,
+        quantity: 10,
+      },
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'Snikers',
+        price: 150,
+        id: 1,
+        quantity: 7,
+      },
+    ],
+  };
+  dataSource: Array<CartItem> = [];
+  constructor(private cartService: CartService) {}
 
   displayedColumns: Array<string> = [
     'product',
     'name',
     'price',
     'quantity',
-     'total',
-     'action'
+    'total',
+    'action',
   ];
 
   ngOnInit(): void {
-    this.dataSource = this.cart.items
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    });
   }
 
   getTotal(items: Array<CartItem>): number {
-    return this._carService.getTotal(items);
+    return this.cartService.getTotal(items);
+  }
+  onClearCart() {
+    this.cartService.clearCart();
   }
 
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { Cart, CartItem } from "../models/cart.model";
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Cart, CartItem } from '../models/cart.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class CartService {
   cart = new BehaviorSubject<Cart>({ items: [] });
@@ -21,9 +21,30 @@ export class CartService {
     }
 
     this.cart.next({ items });
-    this._snakeBar.open("1 item added to cart.", "Ok", { duration: 3000 });
+    this._snakeBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
     console.log(this.cart.value);
   }
+
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined;
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity--;
+        if (_item.quantity == 0) {
+          itemForRemoval = _item;
+        }
+      }
+      return _item;
+    });
+    if (itemForRemoval) {
+      filteredItems = this.removeFromCart(itemForRemoval);
+    }
+    this.cart.next({ items: filteredItems });
+    this._snakeBar.open('1 item removed from cart', 'Ok', {
+      duration: 3000,
+    });
+  }
+
   getTotal(items: Array<CartItem>): number {
     return items
       .map((item) => item.price * item.quantity)
@@ -31,8 +52,18 @@ export class CartService {
   }
   clearCart(): void {
     this.cart.next({ items: [] });
-    this._snakeBar.open("Cart is clear.", "Ok", {
+    this._snakeBar.open('Cart is clear.', 'Ok', {
       duration: 3000,
     });
+  }
+  removeFromCart(item: CartItem, update = true): Array<CartItem> {
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+    if (update) {
+      this.cart.next({ items: filteredItems });
+      this._snakeBar.open('1 item removed from cart', 'Ok', { duration: 3000 });
+    }
+    return filteredItems;
   }
 }
